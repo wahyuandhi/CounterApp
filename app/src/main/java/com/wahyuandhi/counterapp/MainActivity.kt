55a760db2fb9af5b337a8cee56a54a5c33bf5df6
wahyuandhi <wakay.98@gmail.com>
2020-10-31 21:23:40 +0700

    Make function for reset, up and down button

--- app/src/main/java/com/wahyuandhi/counterapp/MainActivity.kt
+++ app/src/main/java/com/wahyuandhi/counterapp/MainActivity.kt
@@ -2,10 +2,46 @@ package com.wahyuandhi.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var values = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnReset = findViewById<Button>(R.id.btn_reset)
+        val tvResult = findViewById<TextView>(R.id.tv_result)
+        val btnUp = findViewById<Button>(R.id.btn_up)
+        val btnDown = findViewById<Button>(R.id.btn_down)
+
+        btnReset.setOnClickListener {
+            reset()
+            tvResult.text = values.toString()
+        }
+
+        btnUp.setOnClickListener {
+            up()
+            tvResult.text = values.toString()
+        }
+
+        btnDown.setOnClickListener {
+            down()
+            tvResult.text = values.toString()
+        }
+    }
+
+    fun reset() {
+        values = 0
+    }
+    fun up() {
+        values++
+    }
+    fun down() {
+        values--
     }
 }
